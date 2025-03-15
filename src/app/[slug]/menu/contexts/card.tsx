683fbd84139c4df905bffa8cbf25511b411a4d0e
Donizete Queiroz <webmaster@zapnetweb.com.br>
2025-03-15 15:03:21 -0300

    feat: ensure same product can  be added more than once  to cart

--- src/app/[slug]/menu/contexts/card.tsx
+++ src/app/[slug]/menu/contexts/card.tsx
@@ -29,7 +29,26 @@ export const CartProvider = ({ children }: {children: ReactNode }) => {
         setIsOpen((prev) => !prev);
     };
     const addProduct = (product: CartProduct) => {
-      setProducts(prev => ([...prev, product]))  
+        //
+        //
+        //
+        const productIsAlreandyOnTheCart = products.some(
+            (prevProduct) => prevProduct.id === product.id,
+        );
+        if (!productIsAlreandyOnTheCart) {
+            return setProducts((prev) => [...prev, product]);
+        }
+        setProducts(prevProducts => {
+            return prevProducts.map((prevProduct) => {
+                if (prevProduct.id === product.id) {
+                    return {
+                        ...prevProduct,
+                        quantity: prevProduct.quantity + product.quantity,
+                    }
+                }
+                return prevProduct
+            })
+        }) 
     };
     return (
         <CartContext.Provider
